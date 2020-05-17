eb80735a80542c7b2cdc22ae8db9e22aac9a0878
Chemi Atlow <chemiatlow@gmail.com>
2020-05-17 17:54:22 +0300

    Add version to env

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,30 @@
+const fs = require("fs");
+const webpack = require("webpack");
+const packageJson = fs.readFileSync("./package.json");
+const version = JSON.parse(packageJson).version || 0;
 module.exports = {
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: `@import "@/utils/_variables.scss";`
-      }
+    configureWebpack: {
+        plugins: [
+            new webpack.DefinePlugin({
+                "process.env": {
+                    PACKAGE_VERSION: '"' + version + '"'
+                }
+            })
+        ]
+    },
+    css: {
+        loaderOptions: {
+            sass: {
+                prependData: `@import "@/utils/_variables.scss";`
+            }
+        }
+    },
+    pwa: {
+        name: "דיווחי מגן דוד אדום",
+        themeColor: "#10133B",
+        msTileColor: "#10133B",
+        manifestOptions: {
+            background_color: "#E0E0E0"
+        }
     }
-  },
-  pwa: {
-    name: 'דיווחי מגן דוד אדום',
-    themeColor: '#10133B',
-    msTileColor: '#10133B',
-    manifestOptions: {
-      background_color: '#E0E0E0'
-    }
-  }
-}
+};
