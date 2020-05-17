ecfefecac5b6fd6ea189362343781c2f3ce36ba2
Chemi Atlow <chemiatlow@gmail.com>
2020-05-17 17:57:42 +0300

    Add the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="grid-container">
+        <HomeButton of="tools">כלי עזר למתנדב</HomeButton>
+        <HomeButton of="moked">דיווחים למוקד</HomeButton>
+        <HomeButton of="contact">צור קשר</HomeButton>
+        <HomeButton of="info">אודות האפליקציה</HomeButton>
+        <HomeButton of="play">הורדה בגוגל פליי</HomeButton>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HomeButton from "@/components/HomeButton.vue";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+    name: "Home",
+    components: {
+        HomeButton
+    }
 };
 </script>
+
+<style lang="scss" scoped>
+.grid-container {
+    display: grid;
+    grid-gap: 0.6rem 0.7rem;
+    grid-template-columns: 50% 50%;
+    align-content: center;
+    margin: 0 auto;
+    width: 90%;
+    max-width: 45rem;
+    .home-btn:first-of-type {
+        grid-column: 1 / span 2;
+    }
+}
+</style>
