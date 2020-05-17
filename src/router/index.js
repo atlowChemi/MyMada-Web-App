2d5b2f3fc7c3645473b29f0b15d64191859ad415
Chemi Atlow <chemiatlow@gmail.com>
2020-05-17 17:58:34 +0300

    add some routes

--- src/router/index.js
+++ src/router/index.js
@@ -5,26 +5,44 @@ import Home from "../views/Home.vue";
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    {
+        path: "/",
+        name: "home",
+        component: Home,
+    },
+    {
+        path: "/info",
+        name: "Info",
+        meta: {
+            title: 'אודות'
+        },
+        component: () => import("../views/Info.vue")
+    },
+    {
+        path: "/privacy",
+        name: "Privacy",
+        meta: {
+            title: 'פרטיות'
+        },
+        component: () => import("../views/Privacy.vue")
+    },
+    {
+        path: "/play",
+        name: "Play",
+        beforeEnter(_to, _from, next) {
+            window.open(
+                "https://play.google.com/store/apps/details?id=atlow.chemi.mymada",
+                "_blank"
+            );
+            next(false);
+        }
+    }
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes
 });
 
 export default router;
