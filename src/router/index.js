36535c2dc1df998445e54e9a9a4718022d76ff33
Chemi Atlow <chemiatlow@gmail.com>
2020-05-17 18:32:48 +0300

    complete the privacy view

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,7 @@ const routes = [
         path: "/privacy",
         name: "Privacy",
         meta: {
-            title: 'פרטיות'
+            title: 'מדיניות פרטיות'
         },
         component: () => import("../views/Privacy.vue")
     },
