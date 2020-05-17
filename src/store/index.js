b5cf64a50918c90bc1b11ba45b8e925d78348169
Chemi Atlow <chemiatlow@gmail.com>
2020-05-17 17:56:34 +0300

    Add version and app-bar

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,13 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+    state: {
+        packageVersion: process.env.PACKAGE_VERSION || "0",
+    },
+    mutations: {},
+    actions: {},
+    modules: {},
+    getters: {
+        appVersion: state => state.packageVersion
+    },
 });
