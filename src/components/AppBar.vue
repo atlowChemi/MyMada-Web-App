b55a3f22737aa65d9fc8755d33ed3fc951857660
Chemi Atlow <chemiatlow@gmail.com>
2020-05-19 14:31:47 +0300

    Add shadow to header, and open settings

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -49,7 +49,7 @@ export default {
     },
     methods: {
         openSettings() {
-            console.log("settings");
+            this.$store.dispatch("alert/settings");
         },
         goBack() {
             this.$router.go(-1);
@@ -62,6 +62,7 @@ export default {
 .main-header {
     $mainHeader: &;
     width: 100%;
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2);
     &__nav {
         text-align: center;
         height: 3.5rem;
