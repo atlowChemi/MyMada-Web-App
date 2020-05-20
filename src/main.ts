7b0119264feb2f753e796c4acc188c54ddf3b69b
Chemi Atlow <chemiatlow@gmail.com>
2020-05-20 10:33:13 +0300

    Add vue-material, and connect to TS

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import Vue from "vue";
 import Waves from "vue-waves-effect";
+import { MdButton, MdCard, MdField, MdRipple } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
 import App from "./App.vue";
 import "./registerServiceWorker";
@@ -10,7 +13,11 @@ import Button from "./components/Button.vue";
 
 Vue.component("app-nav", Navigation);
 Vue.component("app-btn", Button);
-Vue.use(Waves)
+Vue.use(Waves);
+Vue.use(MdButton);
+Vue.use(MdCard);
+Vue.use(MdField);
+Vue.use(MdRipple);
 
 Vue.config.productionTip = false;
 
