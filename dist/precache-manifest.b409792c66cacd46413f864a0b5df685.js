276ec1381857b2b4ecce6797aa0e421fac4f7eff
Chemi Atlow <chemiatlow@gmail.com>
2020-05-21 10:46:52 +0300

    Add Dist to servr on heroku

---
+++ dist/precache-manifest.b409792c66cacd46413f864a0b5df685.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153c8caec5323f3fc592",
+    "url": "/css/app.e153f48c.css"
+  },
+  {
+    "revision": "b6fa168ed9ed66b117a9",
+    "url": "/css/chunk-7fa0e4f1.6f7c2a39.css"
+  },
+  {
+    "revision": "0bb94572aefb19a83e6c",
+    "url": "/css/chunk-cab32aa6.da1346a8.css"
+  },
+  {
+    "revision": "70db2fbbfb02784cbf9f",
+    "url": "/css/chunk-vendors.9c32a4e2.css"
+  },
+  {
+    "revision": "cd77f4da7d6b1ae9f6a4c36d887376bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "153c8caec5323f3fc592",
+    "url": "/js/app.9f4917da.js"
+  },
+  {
+    "revision": "d4046e9d57ef38a7b432",
+    "url": "/js/chunk-2d0b257b.01ed1559.js"
+  },
+  {
+    "revision": "b6fa168ed9ed66b117a9",
+    "url": "/js/chunk-7fa0e4f1.5ae8e955.js"
+  },
+  {
+    "revision": "0bb94572aefb19a83e6c",
+    "url": "/js/chunk-cab32aa6.20283764.js"
+  },
+  {
+    "revision": "70db2fbbfb02784cbf9f",
+    "url": "/js/chunk-vendors.db3639b1.js"
+  },
+  {
+    "revision": "23ae1a172213141c20e72479da2e2c7d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
