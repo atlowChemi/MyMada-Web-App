685f3e7421262f8be6785635f321c5587e97c3b4
Chemi Atlow <chemiatlow@gmail.com>
2020-05-22 08:53:22 +0300

    Add empty state for no messages, start connection to Firebase

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -8,9 +8,15 @@
                     </md-ripple>
                 </md-card>
             </div>
+            <md-empty-state
+                v-if="!seenMsg"
+                md-icon="devices_other"
+                md-label="לא קיימות הודעות"
+                md-description="לא הצלחנו לזהות הודעות שנשלחו ממכשיר זה. תוכל לשלוח לנו הודעה מעמוד זה."
+            ></md-empty-state>
         </div>
         <footer>
-            <b class="seenMsg">טרם נשלחו הודעות</b>
+            <b class="seenMsg" v-if="seenMsg">{{ seenMsg }}</b>
             <div class="container new-msg__container">
                 <div class="new-msg__input">
                     <md-field md-clearable :class="hasErrors">
@@ -20,7 +26,7 @@
                     </md-field>
                 </div>
                 <div class="new-msg__submit">
-                    <app-btn class="long waves-light">
+                    <app-btn class="long waves-light" @click="sendMsg">
                         שלח
                         <i class="material-icons left">send</i>
                     </app-btn>
@@ -30,38 +36,58 @@
     </div>
 </template>
 
-<script>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { ContactDb } from "../utils/firebaseConfig";
+
 const warningMsg = `אנו גוף עצמאי שאינו בקשר עם מד"א. לכן
                 <b>אין</b> לשלוח לנו פניות למקרים דחופים הדורשים מענה רפואי.
                 <br />כמו כן, איננו קשורים לאפליקציית הצוותים. אין ביכולתנו
                 לסייע בה, או לטפל בתקלות בשליחת וקבלת מקרים. <br />עיזרו לנו,
                 ואל תיפנו בנושאים שאינם תחום אחריותנו, בכדי שנוכל לתת לכם את
                 המענה המיטבי!`;
-export default {
-    data() {
+
+@Component
+export default class Contact extends Vue {
+    msg: string | null = null;
+    seenMsg: string = "";
+    get database() {
+        return ContactDb(this.$store.state.user.fcmToken);
+    }
+    get hasErrors() {
+        console.log("D");
         return {
-            msg: null,
+            "md-invalid": this.msg!?.length <= 0,
         };
-    },
+    }
     mounted() {
+        this.database.on("value", snapshot => {
+            if (!snapshot.hasChild("Name")) return;
+            console.log(snapshot.val());
+            if (snapshot.hasChild("SeenByAdmin")) {
+                this.seenMsg = snapshot.val().SeenByAdmin ? "מנהל ראה את ההודעה שלך" : "מנהל טרם ראה את ההודעה";
+            }
+        });
         this.$store.dispatch("alert/error", {
             message: warningMsg,
             title: "הודעה חשובה",
         });
-    },
-    computed: {
-        hasErrors() {
-            return {
-                "md-invalid": this.msg?.length <= 0,
-            };
-        },
-    },
-    methods: {
-        changeName() {
+    }
+    changeName() {
+        this.$store.dispatch("alert/changeName");
+    }
+    sendMsg() {
+        if (!this.$store.getters["user/validUserName"]) {
             this.$store.dispatch("alert/changeName");
-        },
-    },
-};
+            return;
+        }
+        if (!this.msg || this.msg.trim().length <= 0) {
+            this.msg = "";
+            return;
+        }
+        this.msg = this.msg.trim();
+    }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -72,7 +98,7 @@ export default {
         padding-top: 1rem;
         height: calc(100% - 11.5rem);
         @include desktop {
-            height: calc(100% - 8rem);
+            height: calc(100% - 8.5rem);
         }
         .md-card-content {
             padding: 0.5rem;
