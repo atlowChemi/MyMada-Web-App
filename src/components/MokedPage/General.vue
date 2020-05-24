8220742f8f3233a228245c7c90e4bceb9e44eae9
Chemi Atlow <chemiatlow@gmail.com>
2020-05-24 19:46:03 +0300

    Start using general component

---
+++ src/components/MokedPage/General.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <md-field md-clearable :class="hasErrors">
+            <label>הודעה כללית</label>
+            <md-textarea v-model="msg" md-autogrow required></md-textarea>
+            <span class="md-error">לא ניתן לשלוח הודעה ריקה</span>
+        </md-field>
+        <app-btn long @click="send">
+            שלח
+            <i class="material-icons">send</i>
+            <md-tooltip md-direction="bottom">כלי זה פועל באמצעות סמס</md-tooltip>
+        </app-btn>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Emit } from "vue-property-decorator";
+
+@Component
+export default class General extends Vue {
+    msg: string|null = null;
+    get hasErrors() {
+        return {
+            "md-invalid": this.msg!?.trim().length <= 0,
+        };
+    }
+    send() {
+        this.msg = this.msg === null ? "" : this.msg.trim();
+        if(this.msg) {
+            this.MessageSent();
+        }
+    }
+    @Emit()
+    MessageSent() {
+        return this.msg;
+    }
+}
+</script>
+
+<style lang="scss">
+#tab-posts {
+    .md-field .md-textarea {
+        max-height: 10rem;
+    }
+}
+</style>
--- src/views/Moked.vue
+++ src/views/Moked.vue
@@ -1,17 +1,61 @@
 <template>
-    <div>
-        <md-tabs :md-is-rtl="true" :md-swipeable="true" md-alignment="fixed">
-            <md-tab id="tab-forms" md-label="הכתבת טפסים" to="/moked/forms">היי</md-tab>
+    <div class="holder">
+        <md-tabs md-sync-route :md-is-rtl="true" :md-swipeable="true" md-alignment="fixed">
+            <md-tab id="tab-forms" md-label="הכתבת טפסים" to="/moked/forms">
+                    <p>היי</p>
+                    <p>היי</p>
+                    <p>היי</p>
+                    <p>היי</p>
+                    <p>היי</p>
+                    <p>היי</p>
+                    <p>היי</p>
+                    <p>היי</p>
+                    <p>היי</p>
+                    <p>היי</p>
+                    <p>היי</p>
+                    <p>היי</p>
+                    <p>היי</p>
+                    <p>היי</p>
+                    <p>היי</p>
+                    <p>היי</p>
+                    <p>היי</p>
+                    <p>היי</p>
+            </md-tab>
             <md-tab id="tab-pages" md-label="דיווח צוות" to="/moked/team">ויי</md-tab>
-            <md-tab id="tab-posts" md-label="הודעה כללית" to="/moked/general">זיי</md-tab>
+            <md-tab id="tab-posts" md-label="הודעה כללית" to="/moked/general"><general /></md-tab>
         </md-tabs>
-        <md-empty-state md-icon="devices_other" md-label="לא קיימים מוקד" md-description="ההמתנה משתלמת."></md-empty-state>
+        <!-- <md-empty-state md-icon="devices_other" md-label="לא קיימים מוקד" md-description="ההמתנה משתלמת."></md-empty-state> -->
     </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
+import General from "@/components/MokedPage/General.vue";
 
-@Component
+@Component({
+    components: {
+        General
+    }
+})
 export default class Tools extends Vue {}
-</script>
+</script>   
+
+<style lang="scss">
+.holder {
+    height: 100%;
+    .md-tabs {
+        $tabs: &;
+        height: 100%;
+        &-content {
+            height: 100% !important;
+            .md-tabs-container {
+                height: 100%;
+                .md-tab {
+                    max-height: 100%;
+                    overflow-y: auto;
+                }
+            }
+        }
+    }
+}
+</style>
