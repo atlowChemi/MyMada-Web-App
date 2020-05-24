9c0c9c6a0844cc1cb8e899bbc4a9b36aeaefe9bb
Chemi Atlow <chemiatlow@gmail.com>
2020-05-24 19:43:09 +0300

    Add Icon-font of tools. switch to TS. Style all scrollbars of app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
     <div id="app">
         <app-nav />
-        <modal
-            v-if="alert.show"
-            :message="alert.message"
-            :title="alert.title"
-            :type="alert.type"
-            :footer="alert.footerType"
-        />
+        <modal v-if="alert.show" :message="alert.message" :title="alert.title" :type="alert.type" :footer="alert.footerType" />
         <main class="main-main">
             <router-view />
         </main>
@@ -15,30 +9,45 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import { Component, Vue, Watch } from "vue-property-decorator";
 import Modal from "./components/Modal.vue";
 
-export default Vue.extend({
+@Component({
     name: "App",
     components: {
         Modal,
     },
-    watch: {
-        $route(to) {
-            document.title = `דיווחי מד"א${to.meta.title ? " - " + to.meta.title : ""}`;
-        },
-    },
-    computed: {
-        alert() {
-            return this.$store.state.alert;
-        },
-    },
-});
+})
+export default class App extends Vue {
+    get alert() {
+        return this.$store.state.alert;
+    }
+    @Watch("$route", { immediate: true, deep: true })
+    routeChange(to: any): void {
+        document.title = `דיווחי מד"א${to.meta.title ? " - " + to.meta.title : ""}`;
+    }
+}
 </script>
 
 <style lang="scss">
 * {
     box-sizing: border-box;
+    &::-webkit-scrollbar {
+        width: 0.5rem;
+        height: 0.5rem;
+        border-radius: 8px;
+    }
+    &::-webkit-scrollbar-button {
+        display: none;
+    }
+    &::-webkit-scrollbar-corner,
+    &::-webkit-scrollbar-track {
+        background: white;
+    }
+    &::-webkit-scrollbar-thumb {
+        background: $btnDisabledColor;
+        border-radius: 8px;
+    }
 }
 
 html {
@@ -143,6 +152,72 @@ body {
             text-align: left;
             white-space: pre;
             direction: ltr;
+        }
+    }
+}
+</style>
+
+<style lang="scss">
+@font-face {
+    font-family: "atlowApps";
+    src: url("assets/fonts/atlowApps.eot?9xqudu");
+    src: url("assets/fonts/atlowApps.eot?9xqudu#iefix") format("embedded-opentype"), url("assets/fonts/atlowApps.ttf?9xqudu") format("truetype"), url("assets/fonts/atlowApps.woff?9xqudu") format("woff"), url("assets/fonts/atlowApps.svg?9xqudu#atlowApps") format("svg");
+    font-weight: normal;
+    font-style: normal;
+    font-display: block;
+}
+[class^="icon-"],
+[class*=" icon-"] {
+    font-family: "atlowApps" !important;
+    speak: none;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+    font-variant: normal;
+    text-transform: none;
+    line-height: 1;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+.icon {
+    &-als:before {
+        content: "\e900";
+    }
+    &-apgar:before {
+        content: "\e901";
+    }
+    &-bls:before {
+        content: "\e902";
+    }
+    &-contractions:before {
+        content: "\e903";
+    }
+    &-dead:before {
+        content: "\e904";
+    }
+    &-dictionary:before {
+        content: "\e905";
+    }
+    &-glazgow:before {
+        content: "\e906";
+    }
+    &-metronome:before {
+        content: "\e907";
+    }
+    &-oxygen:before {
+        content: "\e908";
+    }
+    &-pulse:before {
+        content: "\e909";
+    }
+    &-vital:before {
+        content: "\e90a";
+    }
+    &-whatsapp {
+        font-size: 5rem;
+        margin: 0;
+        &:before {
+            content: "\e90b";
         }
     }
 }
