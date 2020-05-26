27bb1e61a8a5aaf7a7846e3d858d43aa6672b31f
Chemi Atlow <chemiatlow@gmail.com>
2020-05-26 21:00:18 +0300

    Add startup for metronome page

---
+++ src/components/ToolsPage/Metronome.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="metro__valign">
+        <div class="metro__valign-row">
+            <p class="metro__info">לחץ בכל נקודה להתחלת המטרונום</p>
+            <circular-button metronome>00:00</circular-button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import CircularButton from "@/components/CircularButton.vue";
+
+@Component({
+    components: {
+        CircularButton,
+    },
+})
+export default class Metronome extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.metro__valign {
+    height: 100%;
+    display: flex;
+    margin-left: auto;
+    margin-right: auto;
+    align-items: center;
+    text-align: center;
+    width: 100%;
+    &-row {
+        width: 100%;
+        padding: 0 0.75rem;
+        .metro__info {
+            position: relative;
+            top: -7vh;
+            font-size: 1rem;
+        }
+    }
+}
+</style>
